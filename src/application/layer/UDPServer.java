dc32b706b37b5f2f469399693ff98f9787ef41dc
nikhilaii93 <nikhilverma.ajm@gmail.com>
2015-08-19 00:50:59 +0530

    test

--- src/application/layer/UDPServer.java
+++ src/application/layer/UDPServer.java
@@ -12,7 +12,7 @@ public class UDPServer {
 		
 		byte[] receiveData = new byte[1024];
 		byte[] sendData = new byte[1024];
-		while(true) {
+		while (true) {
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
 			String receiveString = new String(receivePacket.getData());
