3427c862385fe9ed2d2a97047fd6930332cd469d
nipun-gupta <nipun.gupta@ueducation.in>
2015-08-19 23:20:38 +0530

    Enumeration of Connection States

---
+++ src/project/networks/util/ConnectionState.java
@@ -0,0 +1,15 @@
+package project.networks.util;
+
+public enum ConnectionState {
+	LISTEN,
+	SYN_SENT,
+	SYN_RECEIVED,
+	ESTABLISHED,
+	FIN_WAIT_1,
+	FIN_WAIT_2,
+	CLOSE_WAIT,
+	CLOSING,
+	LAST_ACK,
+	TIME_WAIT,
+	CLOSED
+}
