3b98b4cfd3059e3b67e1cba6e269a8aba168843a
nikhilaii93 <nikhilverma.ajm@gmail.com>
2015-08-20 22:06:22 +0530

    getters & setters in TCPHeader

--- src/project/networks/util/TCPHeader.java
+++ src/project/networks/util/TCPHeader.java
@@ -30,4 +30,69 @@ public class TCPHeader {
 		checksum = checksum_;
 		urgent_pointer = urgent_pointer_;
 	}
+	
+	public short getSrc_port() {
+		return src_port;
+	}
+
+	public void setSrc_port(short src_port) {
+		this.src_port = src_port;
+	}
+
+	public short getDst_port() {
+		return dst_port;
+	}
+
+	public void setDst_port(short dst_port) {
+		this.dst_port = dst_port;
+	}
+
+	public int getSeq_num() {
+		return seq_num;
+	}
+
+	public void setSeq_num(int seq_num) {
+		this.seq_num = seq_num;
+	}
+
+	public int getAck_num() {
+		return ack_num;
+	}
+
+	public void setAck_num(int ack_num) {
+		this.ack_num = ack_num;
+	}
+
+	public short getDrf() {
+		return drf;
+	}
+
+	public void setDrf(short drf) {
+		this.drf = drf;
+	}
+
+	public short getWindow_size() {
+		return window_size;
+	}
+
+	public void setWindow_size(short window_size) {
+		this.window_size = window_size;
+	}
+
+	public short getChecksum() {
+		return checksum;
+	}
+
+	public void setChecksum(short checksum) {
+		this.checksum = checksum;
+	}
+
+	public short getUrgent_pointer() {
+		return urgent_pointer;
+	}
+
+	public void setUrgent_pointer(short urgent_pointer) {
+		this.urgent_pointer = urgent_pointer;
+	}
+
 }
