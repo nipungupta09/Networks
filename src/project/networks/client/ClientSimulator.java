7ca4ca88c27e33e6e546cde5af0cbc4a1ef6ad87
nipun-gupta <nipun.gupta@ueducation.in>
2015-09-30 04:44:27 +0530

    Starting feature

--- src/project/networks/client/ClientSimulator.java
+++ src/project/networks/client/ClientSimulator.java
@@ -1,8 +1,6 @@
 package project.networks.client;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import project.networks.connection.UDPClientSender;
 
 public class ClientSimulator {
 	
@@ -10,22 +8,22 @@ public class ClientSimulator {
 	private static final int SERVER_PORT = 9876;
 	
 	public static void main(String[] args) throws Exception {
-//		String dummyData = "Dummy Data";
-//		byte[] receiveData = UDPClientSender.send(dummyData.getBytes(), SERVER_IP_ADDRESS, SERVER_PORT);
-//		System.out.println("ACK received from server: " + new String(receiveData));
-		
 		String dummyData = "Dummy Data";
-		for(int i=0; i<5000; i++)
-			dummyData = dummyData + "Dummy Data";
-		byte[] data = dummyData.getBytes();
-		System.out.println(data.length);
-		DatagramSocket clientSocket = new DatagramSocket();
-		DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getByName(SERVER_IP_ADDRESS), SERVER_PORT);
-		long t1 = System.nanoTime();
-		for(int i=0; i<100000; i++)
-			clientSocket.send(sendPacket);
-		System.out.println(System.nanoTime()-t1);
-		clientSocket.close();
+		byte[] receiveData = UDPClientSender.send(dummyData.getBytes(), SERVER_IP_ADDRESS, SERVER_PORT);
+		System.out.println("ACK received from server: " + new String(receiveData));
+		
+//		String dummyData = "Dummy Data";
+//		for(int i=0; i<5000; i++)
+//			dummyData = dummyData + "Dummy Data";
+//		byte[] data = dummyData.getBytes();
+//		System.out.println(data.length);
+//		DatagramSocket clientSocket = new DatagramSocket();
+//		DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getByName(SERVER_IP_ADDRESS), SERVER_PORT);
+//		long t1 = System.nanoTime();
+//		for(int i=0; i<100000; i++)
+//			clientSocket.send(sendPacket);
+//		System.out.println(System.nanoTime()-t1);
+//		clientSocket.close();
 		
 	}
 
--- src/project/networks/server/UDPServerReceiver.java
+++ src/project/networks/server/UDPServerReceiver.java
@@ -6,7 +6,7 @@ import java.net.DatagramSocket;
 public class UDPServerReceiver {
 
 	public static final int SERVER_PORT = 9876;
-	public static int counter = 0;
+//	public static int counter = 0;
 	
 	public static void main(String[] args) throws Exception {
 		DatagramSocket serverSocket = new DatagramSocket(SERVER_PORT);
@@ -15,8 +15,8 @@ public class UDPServerReceiver {
 		while(true) {
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			serverSocket.receive(receivePacket);
-			System.out.println(++counter);
-//			UDPServerSender.send(receivePacket, serverSocket);
+//			System.out.println(++counter);
+			UDPServerSender.send(receivePacket, serverSocket);
 		}		
 	}
 
